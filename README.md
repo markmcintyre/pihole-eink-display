1afde3c770c4a1be6f20a876aee7cb60c192150c
Mark McIntyre <mark@markmcintyre.ca>
2022-02-06 16:04:01 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Simple Pi-hole Stats Display
+
+![Alt text](images/pihole-eink-display.jpg?raw=true "Photo of Pi-hole eInk Display")
+
+This Python script outputs Pi-hole statistics on a 2.7" e-ink display, and updates it periodically.
+
+Note: This is just a statistics display script, not the actual DNS Sinkhole. If you're looking to set up a DNS sinkhole, check out the [Pi-hole project page](https://pi-hole.net), totally unaffiliated with this script.
+
+The display I use is from [WaveShare](https://www.waveshare.com), and this script uses [their library](https://www.waveshare.com/wiki/2.7inch_e-Paper_HAT). (Instructions on how to install their library can be found in the previous link.)
+
+## Configuring the script
+
+There are three variables to customize, which are near the top of ``main.py``.
+
+PIHOLE_IP
+   : The IP address of your Pi-hole instances. (Use ``localhost`` if it's running on the same Pi as the PiHole).
+
+DISPLAY_TITLE
+   : A title to display at the top of your stats. Customize it to your liking!
+
+REFRESH_MINUTES
+   : The number of minutes between refreshes. I use five minutes.
+
+## Fonts
+
+The fonts folder includes the open-source [Roboto](https://github.com/googlefonts/roboto) font family.
+
+Enjoy! 👍
---
+++ main.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import epd2in7
+import time
+import datetime
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+import json
+import urllib.request
+from threading import Timer
+import os
+
+# CUSTOM VARIABLES
+PIHOLE_IP = 'pihole.local'
+DISPLAY_TITLE = 'PiHole Statistics'
+REFRESH_MINUTES = 5
+
+# Get our path
+dirname = os.path.dirname(__file__)
+
+# Prepare some fonts and images
+blackHoleImage = Image.open(os.path.join(dirname, 'images/black-hole.png'))
+smallBlackHoleImage = blackHoleImage.resize((32, 27))
+titleFont = ImageFont.truetype(os.path.join(dirname, 'fonts/Roboto-Bold.ttf'), 22)
+font = ImageFont.truetype(os.path.join(dirname, 'fonts/Roboto-Thin.ttf'), 16)
+metricFont = ImageFont.truetype(os.path.join(dirname, 'fonts/Roboto-Regular.ttf'), 32)
+descriptionFont = ImageFont.truetype(os.path.join(dirname, 'fonts/Roboto-Thin.ttf'), 14)
+
+def renderData(canvas, data):
+
+    # Elements
+    draw = ImageDraw.Draw(canvas)
+    today = datetime.date.today()
+    width = canvas.width
+    height = canvas.height
+
+    # Display our header, with black hole icon and the date
+    canvas.paste(smallBlackHoleImage, (8, 16))
+    draw.text((50, 7), DISPLAY_TITLE, font = titleFont, fill = 0)
+    draw.text((50, 32), '{:%A, %B %-d}'.format(today), font = font, fill = 0)
+    draw.line((0, 60, width, 60), fill = 0, width = 3)
+
+    # Display our grid
+    draw.line((width / 2, 60, width / 2, height))
+    draw.line((0, ((height - 60) / 2) + 60, width, ((height - 60) / 2) + 60))
+
+    # Display the number of ads blocked
+    draw.text((5, 63), '{:,}'.format(data['ads_blocked_today']), font = metricFont)
+    draw.text((5, 98), 'Ads blocked today', font = descriptionFont)
+
+    # Display number of queries
+    draw.text((width / 2 + 10, 63), '{:,}'.format(data['dns_queries_today']), font = metricFont)
+    draw.text((width / 2 + 10, 98), 'DNS queries today', font = descriptionFont)
+
+    # Display the percentage of ads
+    draw.text((5, ((height - 60) / 2) + 63), '{:.2f}%'.format(data['ads_percentage_today']), font = metricFont)
+    draw.text((5, ((height - 60) / 2) + 98), 'Ad percentage', font = descriptionFont)
+
+    # Display devices protected
+    draw.text(((width / 2 + 10), ((height - 60) / 2 + 63)), '{0:d}'.format(data['unique_clients']), font = metricFont)
+    draw.text(((width / 2 + 10), ((height - 60) / 2 + 98)), 'Devices protected', font = descriptionFont)
+
+try:
+
+    # Update every x minutes
+    interval = REFRESH_MINUES * 60
+
+    # Initialize our ePaper Display
+    epd = epd2in7.EPD()
+    
+    # Prepare our canvas
+    width = epd2in7.EPD_HEIGHT
+    height = epd2in7.EPD_WIDTH
+    
+    # Define our main loop function
+    def loop():
+
+        # Initialize the ePaper display
+        epd.init()
+        epd.Clear(0xFF)
+
+        # (Re-)create our canvas
+        canvas = Image.new('1', (width, height), 255)
+
+        try:
+            # Get our ad blocker data
+            response = urllib.request.urlopen(f'http://{PIHOLE_IP}/admin/api.php')
+            data = json.loads(response.read().decode())
+
+            # Render the data
+            renderData(canvas, data)
+            epd.display(epd.getbuffer(canvas))
+
+        except urllib.request.URLError as err:
+            print('Could not get API data.')
+
+        # Put our display to sleep
+        epd.sleep()
+
+        # Do it again at the next interval
+        Timer(interval, loop).start()
+
+    loop()
+        
+except:
+    print('traceback.format_exc():\n%s',traceback.format_exc())
+    exit()
+
